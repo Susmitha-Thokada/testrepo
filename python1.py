62db586bffa89f8289dc2dffeb906c11d8eea1db
SUSMITHA <142213217+Susmitha-Thokada@users.noreply.github.com>
2024-06-13 14:13:34 +0530

    Add files via upload
    Python File

---
+++ python1.py
@@ -0,0 +1,85 @@
+#This is a comment
+print("Hello, Welcome to my world....")
+
+#Simple Example using variables
+'''Variable conditions:
+          Special charecters are not accepted
+          Numbers, Space, Keywords like (int,float..)are not accepted
+          If variable name contains two words Underscore_ is used'''
+a=12
+print(a)#prints the value of a
+
+
+a ,b ,c ,d = 2 ,2.5 ,True ,"Hello"
+print(type(a))#The type is defined the data type of 'a'
+print(type(b))#The type is defined the data type of 'b'
+print(type(c))#The type is defined the data type of 'c'
+print(type(d))#The type is defined the data type of 'd'
+
+
+X="Sushmi"
+Y=85
+Z="Python is awesome.."
+print(X,Y,Z)#prints the values that are stored in X,Y,Z  (or)
+
+print(X)
+print(Y)
+print(Z)
+
+'''Data types in Python
+   It has : integer (int)
+            float   (float)
+            boolean (bool)
+            string  (str)
+Here for multiple lines of comments we use (''' ''' (or) """ """) '''
+
+#Integer
+Rolex=95
+A = -10
+B = 0
+c = 12345678987654321
+print(Rolex)
+print(type(Rolex))
+print(A)
+print(type(A))
+print(B)
+print(type(B))
+print(c)
+print(type(c))# Integer takes the all the numbers including zero and negative numbers
+
+
+#Float
+cse_Toppers = 1234.56778#Float doesn't accept double dots(.)
+geeTha = 25.00
+print(cse_Toppers)
+print(type(cse_Toppers))
+print(geeTha)
+
+
+#Boolean
+X = True
+Y = False
+print(type(X))
+print(Y)
+
+#String
+College1 = "Dadi institute of engineering & technology"
+Char = 'S'  # In python Single char or multiple lines of char stores in String only
+print(College1)
+print(type(College1))
+print(Char)
+print(type(Char))
+
+
+#single , double, triple quotations
+#To print a multi-line string in Python we use triple quotes.('''  ''' or """  """) Example
+print(""" What’s up Internet!
+I’m Manish from RebellionRider.com. 
+Keep watching these tutorials.
+And, you will be the master of “Python Programming” very soon. """)
+
+
+
+
+
+
